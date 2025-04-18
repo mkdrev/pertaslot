03e1069c654640d1a041adadf4369a0f9c5ac7e3
mkdrev <mikedeon17@gmail.com>
2025-04-18 14:21:14 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.environ.get("BOT_TOKEN")  # Gunakan nama env yang aman dan jelas
+app = ApplicationBuilder().token(TOKEN).build()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+
+    await context.bot.send_photo(
+        chat_id=chat_id,
+        photo="https://i.pinimg.com/736x/99/9b/b8/999bb8d77251045efa7364b96c170509.jpg",
+        caption="Halo Bosku, selamat datang di *PERTASLOT!*\n\n"
+                "Pilih menu di bawah untuk mulai bermain atau cek fitur lainnya.",
+        parse_mode="Markdown"
+    )
+
+    keyboard = [
+        [InlineKeyboardButton("🎮 PLAY", web_app=WebAppInfo(url="https://kayapertaslot.com/register/K5PRL9B2"))],
+        [InlineKeyboardButton("🎰 RTP GACOR", web_app=WebAppInfo(url="https://kayapertaslot.com/register/K5PRL9B2"))],
+        [InlineKeyboardButton("🎁 PROMOTION", web_app=WebAppInfo(url="https://kayapertaslot.com/register/K5PRL9B2"))],
+        [InlineKeyboardButton("📲 SOCIAL MEDIA", web_app=WebAppInfo(url="https://kayapertaslot.com/register/K5PRL9B2"))],
+        [InlineKeyboardButton("💬 LIVECHAT", web_app=WebAppInfo(url="https://kayapertaslot.com/register/K5PRL9B2"))],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await context.bot.send_message(
+        chat_id=chat_id,
+        text="Pilih salah satu opsi berikut:",
+        reply_markup=reply_markup
+    )
+
+if __name__ == '__main__':
+    print("[✅] Bot PERTASLOT is running...")
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
